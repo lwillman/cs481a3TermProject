5e20b56eb764ef27fc6ca0358921065dd666ba27
Lacey Willmann <31229321+lwillman@users.noreply.github.com>
2020-04-16 11:45:12 -0600

    Update Patient.sol

--- contracts/Patient.sol
+++ contracts/Patient.sol
@@ -1,13 +1,18 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 import "./safemath.sol";
+import "./UserEntity.sol";
 
 contract Patient {
     using SafeMath for uint256;
     using SafeMath for uint32;
     using SafeMath for uint16;
 
-    event NewPatient(string virus, string longitude, string latitude, uint ageGroup, bool gender);
+    event NewPatient(string virus, string location, string longitude, string latitude, uint ageGroup, bool gender);
+    
+   /* modifier canWrite() {
+        require()
+    }*/
 
     /*
         AgeGroup { 1 = 0-10,
@@ -20,6 +25,7 @@ contract Patient {
     */
     struct Patient {
         string virus;
+        string location;
         string longitude;
         string latitude;
         uint ageGroup;
@@ -29,19 +35,33 @@ contract Patient {
     Patient[] public patients;
     
     //mapping(address => uint) countryPatientCount;
-    mapping(uint => string) public patientToVirus;
+    mapping(string => Patient[]) public patientsByLocation;
+    mapping(string => Patient[]) public patientsByVirus;
     mapping(string => uint) public virusPatientCount;
+    mapping(address => bool) public authorizedEntity;
+    
+    // Modifier for permissible reporting entities
+    modifier isAuthorized(){
+        require(authorizedEntity[msg.sender], "You do not have authorization to modify patient records.");
+        _;
+    }
+    
+    function _addUser() public {
+        authorizedEntity[msg.sender] = true;
+    }
 
-   function _createPatient(string memory _virus, string memory _longitude, string memory _latitude, uint _ageGroup, bool _gender) public {
-        uint patientId = patients.push(Patient(_virus, _longitude, _latitude, _ageGroup, _gender));
-        bytes32 virusID = keccak256(string_tobytes(_virus));
-        patientToVirus[patientId - 1] = _virus;
+   function _createPatient(string memory _virus, string memory _location, string memory _longitude, string memory _latitude, uint _ageGroup, bool _gender) public isAuthorized {
+        uint patientId = patients.push(Patient(_virus, _location, _longitude, _latitude, _ageGroup, _gender));
+        
         virusPatientCount[_virus] = virusPatientCount[_virus].add(1);
-        emit NewPatient(_virus, _longitude, _latitude, _ageGroup, _gender);
+        patientsByLocation[_location].push(Patient(_virus, _location, _longitude, _latitude, _ageGroup, _gender));
+        patientsByVirus[_virus].push(Patient(_virus, _location, _longitude, _latitude, _ageGroup, _gender));
+        emit NewPatient(_virus, _location, _longitude, _latitude, _ageGroup, _gender);
    }
 
-    function string_tobytes(string memory s) internal pure  returns (bytes memory){
-        bytes memory b3 = bytes(s);
-        return b3;
+    function patientsPerLocation(string memory location) public view returns(uint) {
+        return patientsByLocation[location].length;
     }
+    
 }
+    

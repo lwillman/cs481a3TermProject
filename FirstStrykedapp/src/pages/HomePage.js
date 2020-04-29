12b0a1fd941cece1ccee09cd001d8a09e92eb53f
SajaSQ <saja.alqurashi@gmail.com>
2020-04-29 15:42:11 -0600

    y

--- FirstStrykedapp/src/pages/HomePage.js
+++ FirstStrykedapp/src/pages/HomePage.js
@@ -17,16 +17,13 @@ class Greeting extends Component {
           Welcome to the   <b> Stryke</b> Dapp!
         </h2>
         <br />
-        <img src="static/Homepage.png" style={imgStyle} width="400px" alt="Zombie rising from grave" />
+        <img src="static/images/Homepage.png" style={imgStyle} width="400px" alt="Zombie rising from grave" />
         <br /> <br />
         <p style={{ textAlign: "center" }}>
-          This CS481A3 blockchain game allows you to create one original zombie
-          on the Ethereum blockchain.
-          <br /> Once created, your zombie can eat cryptokitties to create new
-          zombies.
-          <br /> Additionally, your zombie can attack other zombies (you have a
-          70% chance of winning).
-          <br /> <br /> To get started, select a button from the menu bar above.
+
+          <br />
+          <br />
+          <br /> <br />
         </p>
       </div>
     );

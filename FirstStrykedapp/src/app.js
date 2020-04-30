7152c25de4f0da565ea3cd59f71852e55cd3d53b
aneub-colostate <63561408+aneub-colostate@users.noreply.github.com>
2020-04-30 14:09:09 -0600

    Create app.js

---
+++ FirstStrykedapp/src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import NavigationBar from "./components/TopBar";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <NavigationBar/>
+            </div>
+        );
+    }
+}
+
+export default App;

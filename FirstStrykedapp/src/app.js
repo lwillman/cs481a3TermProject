6a0e0974be024a9c198bd8b477289d95c91997c7
SajaSQ <saja.alqurashi@gmail.com>
2020-04-30 19:41:13 -0600

    commit

--- FirstStrykedapp/src/app.js
+++ FirstStrykedapp/src/app.js
@@ -1,65 +1,42 @@
+
 import React, { Component } from "react";
 import getWeb3 from "./utils/getWeb3";
 import initBlockchain from "./utils/initBlockchain";
-import getPatientsCount from "./utils/getPatientsCount";
-
+import getPatientCounte from "./utils/getPatientCounteCount";
 
 import { HashRouter, Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import { Provider } from "react-redux";
-
-
-import TopBar from "./components/TopBar";
-import home from "./pages/home";
-import DataEntry from "./pages/DataEntry";
-
-
-
-// import Homepage from "./pages/home";
-// import Data from "./pages/DataEntry";
-
-import store from "./redux/store";
-
-
-
 class App extends Component {
 
+//import components
+import Home from "./components/home";
+import FileUpload from "./components/FileUpload";
+import editable from "./components/editable";
 
 
-  componentDidMount = async () => {
-    try {
-      const web3 = await getWeb3(); // from utils directory;  connect to metamask
-      const data = await initBlockchain(web3);  // get contract instance and user address
-      await getPatientCount(data.CZ, data.virus);  // get user count and total count of getpatients
 
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`
-      );
-
-      console.log(error);
-    }
-  };
-
-  render() {
-    return (
-      <Provider store={store}>
-        <HashRouter>
-          <Container>
-            <TopBar state={this.state} />
-            <div>
-              //back to this agian
-
-              <Route exact path="/home" component={home} />
-              <Route exact path="/DataEntry" component={DataEntry} />
-
-            </div>
-          </Container>
-        </HashRouter>
-      </Provider>
+componentDidMount = async () => {
+  try {
+    const web3 = await getWeb3();
+    const data = await initBlockchain(web3);
+    await getPatientCounte(data.CZ, data.virus);
+  } catch (error) {
+    // Catch any errors for any of the above operations.
+    alert(
+      `Failed to load web3, accounts, or contract. Check console for details.`
     );
+
+    console.log(error);
   }
-}
+};
+
+
+
+
+
+  
 
 export default App;
+
+}
---
+++ FirstStrykedapp/src/componant/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+let Home = React.createClass({
+    render: function() {
+        return (
+            <div>
+            <img src="images/worldmap.jpg" alt="Hi!" width="102%" height="102%"/>
+            </div>
+    );
+    }
+});
+
+export default Home;
